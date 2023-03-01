b8afefd9456c336b0566e8fec5f0d308940bc5f4
Vladislav Kolyanichenko <aljkeee@gmail.com>
2023-03-01 05:50:36 +0300

    Revert "feat: добавить класс Fact, получение данных из БД"
    This reverts commit d0bc6a24f3320d1ab493f432fb0ac197f578160a.

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,53 @@
-import { varConfig } from "./conf.js";
+import { facts, varConfig } from "./conf.js";
 import BurgerMenu from "./Burger-menu.js"
-import Fact from "./Fact.js";
 
-const getFact = new Fact(varConfig);
+function openPopup() {
+  varConfig.popupSelector.classList.add('popup_opened');
+  varConfig.popupAnswerTextSelector.textContent = facts[varConfig.randomedNumber].answer;
+}
+
+function closePopup() {
+  varConfig.popupSelector.classList.remove('popup_opened');
+  varConfig.popupContentSelector.setAttribute('class', 'popup__content');
+  renderQuestion();
+}
+
+function renderQuestion() {
+  varConfig.randomedNumber = Math.floor(Math.random() * facts.length);
+  varConfig.questionTextSelector.textContent = facts[varConfig.randomedNumber].question;
+  varConfig.buttonPositive.addEventListener('click', checkAnswer);
+  varConfig.buttonNegative.addEventListener('click', checkAnswer);
+}
+
+function showAnswer(evt, classResult, textResult) {
+  evt.target.removeEventListener('click', checkAnswer);
+  varConfig.popupContentSelector.classList.add(classResult);
+  varConfig.popupHeadTextSelector.textContent = textResult;
+  (classResult === varConfig.typeSuccessClass) ? varConfig.counterCorrect += 1 : varConfig.counterIncorrect += 1;
+  varConfig.countCorrectSelector.textContent = varConfig.counterCorrect;
+  varConfig.countIncorrectSelector.textContent = varConfig.counterIncorrect;
+  openPopup();
+}
+
+function checkAnswer(evt) {
+  if (facts[varConfig.randomedNumber].isTrue) {
+    (evt.target === varConfig.buttonPositive)
+      ? showAnswer(evt, varConfig.typeSuccessClass, varConfig.textResult.successTrueFact)
+      : showAnswer(evt, varConfig.typeFailClass, varConfig.textResult.failTrueFact)
+  } else {
+    (evt.target === varConfig.buttonPositive)
+      ? showAnswer(evt, varConfig.typeFailClass, varConfig.textResult.failFalseFact)
+      : showAnswer(evt, varConfig.typeSuccessClass, varConfig.textResult.successFalseFact)
+  }
+};
+
+varConfig.buttonNextQuestion.addEventListener('click', closePopup);
+
+renderQuestion();
+
 const activeBergerMenu = () => {
   const burgerMenu = new BurgerMenu();
   return burgerMenu.activateBurgerMenu();
 }
 
 activeBergerMenu();
-
-
-getFact.enableFactGenerator();
