45853474bdd8d13a9c8efac329baab135fded0f4
Vladislav Kolyanichenko <aljkeee@gmail.com>
2023-03-11 12:14:34 +0300

    Revert "feat: создать PopupStart и подключить таймер"
    This reverts commit 4344b47590e4593ee9f28bb2a4174efb54717bf7, reversing
    changes made to d008ac1dd778fed4081e8ae51c96baf6b2bc08ef.

--- scripts/index.js
+++ scripts/index.js
@@ -2,31 +2,17 @@ import { varConfig } from './conf.js';
 import BurgerMenu from './Burger-menu.js';
 import Fact from './Fact.js';
 import { PopupWithFact } from './PopupWithFact.js';
-import { PopupWithResult } from './PopupWithResult.js';
-import { PopupStart } from './PopupStart.js';
 
 const getFact = new Fact(varConfig); // Получаем экземпляр класса Fact. Метод: .fetchRandomFact();
 const burgerMenu = new BurgerMenu(); // Получаем экземпляр класса BurgerMenu. Метод: .activateBurgerMenu();
 const popupWithFact = new PopupWithFact(
   '.popup_type_fact',
   varConfig,
-  getFact.getCheckedFact,
-  getFact.getTextAnswer,
+  getFact.checkAnswer,
+  getFact.returnAnswer,
   getFact.fetchRandomFact
-);
-const popupWithResult = new PopupWithResult(
-  '.popup_type_result',
-  popupWithFact.getCorrectCounter,
-  popupWithFact.getIncorrectCounter,
-  varConfig
-);
-const popupStart = new PopupStart(
-  '.popup_type_start',
-  varConfig,
-  popupWithResult.timer
-);
+); // Получаем экземпляр класса. Вторым аргументом передаем результат проверки ответа
 
 burgerMenu.activateBurgerMenu();
 getFact.fetchRandomFact();
 popupWithFact.setEventListeners();
-popupStart.start();
