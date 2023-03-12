f1e206132482338c497ef687d9bd06921a83b2f4
Vladislav Kolyanichenko <aljkeee@gmail.com>
2023-03-12 09:26:04 +0300

    feat: добавить авторизацию и регистрацию

--- scripts/Burger-menu.js
+++ scripts/Burger-menu.js
@@ -2,10 +2,12 @@ export default class BurgerMenu {
   constructor() {
     this._menu = document.querySelector('.burger-menu');
     this._toggle = this._menu.querySelector('.burger-menu__toggle');
+    this._sectionProfile = document.querySelector('.section-profile');
   }
 
   _closeBurgerMenu() {
     this._menu.classList.toggle('burger-menu_active');
+    this._sectionProfile.classList.toggle('section-profile_hide');
   }
 
   _setEventListener() {
--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,57 @@ import PopupWithForm from './PopupWithForm.js';
 import Form from './Form.js';
 import TabList from './TabList.js';
 
+// тест входа
+const profileAuth = document.querySelector('.profile_user');
+const profileGuest = document.querySelector('.profile_guest');
+const profileName = document.querySelectorAll('.profile__nickname');
+
+const formAuth = document.forms['autorization'];
+const inputAuthLogin = formAuth.querySelector('.form__input_type_login');
+const inputAuthPassword = formAuth.querySelector('.form__input_type_password');
+
+const formReg = document.forms['registration'];
+const inputRegLogin = formReg.querySelector('.form__input_type_login');
+const inputRegPassword = formReg.querySelector('.form__input_type_password');
+
+
+const btnLogout = document.querySelector('.button_type_logout');
+
+const state = {
+  formData: {
+    "username": "",
+    "password": ""
+  },
+  resData: {}
+};
+
+const authRender = () => {
+  profileAuth.classList.add('profile_active');
+  profileGuest.classList.remove('profile_active');
+  profileName.forEach(nickname => nickname.textContent = localStorage.username)
+
+};
+
+const guestRender = () => {
+  profileGuest.classList.add('profile_active');
+  profileAuth.classList.remove('profile_active');
+  profileName.forEach(nickname => nickname.textContent = 'Гость')
+};
+
+const renderPage = (token) => {
+  token ? authRender() : guestRender()
+};
+
+inputAuthLogin.addEventListener('change', (evt) => { state.formData["username"] = evt.target.value; console.log(state.formData) });
+inputAuthPassword.addEventListener('change', (evt) => { state.formData["password"] = evt.target.value; console.log(state.formData) });
+inputRegLogin.addEventListener('change', (evt) => { state.formData["username"] = evt.target.value; console.log(state.formData) });
+inputRegPassword.addEventListener('change', (evt) => { state.formData["password"] = evt.target.value; console.log(state.formData) });
+btnLogout.addEventListener('click', () => {
+  delete localStorage.username;
+  delete localStorage.token;
+  renderPage(localStorage.token);
+})
+
 const profileLinkAvatar = document.querySelector('.profile__link');
 
 const getFact = new Fact(varConfig); // Получаем экземпляр класса Fact. Метод: .fetchRandomFact();
@@ -20,15 +71,43 @@ const popupWithFact = new PopupWithFact(
 
 const formAutorization = new Form(
   'autorization',
-  () => {
+  async () => {
     popupAutorization.close();
-    console.log(formRegistration.getInputValues());
+    await fetch('http://45.146.165.205:3000/auth/login', {
+      headers: {
+        "Content-type": "application/json"
+      },
+      method: "POST",
+      body: JSON.stringify(state.formData)
+    })
+      .then(res => res.json())
+      .then(data => state.resData = data)
+
+    localStorage.setItem('token', state.resData.token);
+    localStorage.setItem('username', state.resData.user.username);
+
+    renderPage(localStorage.token);
+
+    console.log(state.resData)
+    console.log(formAutorization.getInputValues());
   }
 );
+
 const formRegistration = new Form(
   'registration',
-  () => {
+  async () => {
     popupAutorization.close();
+    await fetch('http://45.146.165.205:3000/auth/registration', {
+      headers: {
+        "Content-type": "application/json"
+      },
+      method: "POST",
+      body: JSON.stringify(state.formData)
+    })
+      .then(res => res.json())
+      .then(data => state.resData = data)
+
+    console.log(state.resData)
     console.log(formRegistration.getInputValues());
   }
 );
@@ -49,3 +128,5 @@ popupWithFact.setEventListeners();
 profileLinkAvatar.addEventListener('click', () => {
   popupAutorization.open();
 });
+
+renderPage(localStorage.token);
